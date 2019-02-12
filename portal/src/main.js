7c5f3c7c318743ffd9e4680b4e78b264488b60a7
Benjamin Sjolander <blsjol16@student.uia.no>
2019-02-12 11:51:54 +0100

    added custom scss file

--- portal/src/main.js
+++ portal/src/main.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-import 'bulma/css/bulma.css'
+import './assets/sass/app.scss'
 
 Vue.config.productionTip = false
 

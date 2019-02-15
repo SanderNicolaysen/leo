4ed94321bbb13ecd32d52e05f964106a0e34aa5a
Sander Nicolaysen <sanden16@uia.student.no>
2019-02-15 14:49:43 +0100

    beautified code

--- portal/src/components/Box.vue
+++ portal/src/components/Box.vue
@@ -1,24 +1,24 @@
 <template>
- <div class="box">
-          <article class="media">
-            <div class="media-left">
-              <span class="icon">
-                <i class="fas fa-passport is-size-3"></i>
-              </span>
-            </div>
-            <div class="media-content">
-              <div class="content">
-                <p>
-                  <strong>{{ title }}</strong>
-                  <br>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean efficitur sit amet massa fringilla
-                  egestas. Nullam condimentum luctus turpis.
-                </p>
-              </div>
-            </div>
+  <div class="box">
+    <article class="media">
+      <div class="media-left">
+        <span class="icon">
+          <i class="fas fa-passport is-size-3"></i>
+        </span>
+      </div>
+      <div class="media-content">
+        <div class="content">
+          <p>
+            <strong>{{ title }}</strong>
+            <br>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean efficitur sit amet massa fringilla
+            egestas. Nullam condimentum luctus turpis.
+          </p>
+        </div>
+      </div>
 
-          </article>
-        </div> <!-- ./box -->
+    </article>
+  </div> <!-- ./box -->
 </template>
 
 <script>
@@ -30,7 +30,6 @@ export default {
   ],
   data () {
     return {
-
     }
   }
 }
--- portal/src/views/Start.vue
+++ portal/src/views/Start.vue
@@ -1,41 +1,41 @@
 <template>
-<div>
-<div class="container">
-  <div class="block has-text-centered">
-    <button class="button is-success is-size-2 is-uppercase">Trekk kølapp</button>
-  </div>
+  <div>
+    <div class="container">
+      <div class="block has-text-centered">
+        <button class="button is-success is-size-2 is-uppercase">Trekk kølapp</button>
+      </div>
 
-  <div class="notification is-info">
-    <button class="delete"></button>
-    <p class="has-text-centered is-size-5">Eller, gjør noen steg selv slik at vi kan hjelpe deg raskere! <br>
-      Begynn med å velge henvedelse under</p>
-  </div>
+      <div class="notification is-info">
+        <button class="delete"></button>
+        <p class="has-text-centered is-size-5">Eller, gjør noen steg selv slik at vi kan hjelpe deg raskere! <br>
+          Begynn med å velge henvedelse under</p>
+      </div>
 
-  <div class="columns">
-    <div class="column">
-      <Box v-bind:title="'Pass'" />
-    </div>
-    <div class="column">
-      <Box v-bind:title="'Anmelde tyveri eller skadeverk'" />
-    </div>
-    <div class="column">
-      <Box v-bind:title="'Hente beslag'" />
-    </div>
-  </div>
+      <div class="columns">
+        <div class="column">
+          <Box v-bind:title="'Pass'" />
+        </div>
+        <div class="column">
+          <Box v-bind:title="'Anmelde tyveri eller skadeverk'" />
+        </div>
+        <div class="column">
+          <Box v-bind:title="'Hente beslag'" />
+        </div>
+      </div>
 
-  <div class="columns">
-    <div class="column">
-      <Box v-bind:title="'Bot'" />
-    </div>
-    <div class="column">
-      <Box v-bind:title="'Våpen'" />
-    </div>
-    <div class="column">
-      <Box v-bind:title="'Avhør'" />
+      <div class="columns">
+        <div class="column">
+          <Box v-bind:title="'Bot'" />
+        </div>
+        <div class="column">
+          <Box v-bind:title="'Våpen'" />
+        </div>
+        <div class="column">
+          <Box v-bind:title="'Avhør'" />
+        </div>
+      </div>
     </div>
   </div>
-</div>
-</div>
 </template>
 
 <script>

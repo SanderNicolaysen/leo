3fd70474061cee0a54668ae273a13cbeae6aef16
Sander Nicolaysen <sanden16@uia.student.no>
2019-02-15 11:47:39 +0100

    fixed size of images

--- portal/src/views/Start.vue
+++ portal/src/views/Start.vue
@@ -1,185 +1,186 @@
 <template>
-  <div>
-    <!-- NAVBAR -->
-    <nav class="navbar block" role="navigation" aria-label="main navigation">
-      <div class="navbar-brand">
+<div class="wrapper">
+  <!-- NAVBAR -->
+  <nav class="navbar block" role="navigation" aria-label="main navigation">
+    <div class="navbar-brand">
+      <a class="navbar-item">
+        <img src="../assets/icons/politilogo.jpg" width="38" height="35">
+      </a>
+      <span role="button" class="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navMenu">
+        <span></span>
+        <span></span>
+        <span></span>
+      </span>
+    </div>
+
+    <div id="navMenu" class="navbar-menu">
+      <div class="navbar-start">
+        <a class="navbar-item is-uppercase is-size-3 has-text-gret-dark">POLITIVAKTA</a>
+      </div>
+
+      <div class="navbar-end">
         <a class="navbar-item">
-          <img src="../assets/icons/politilogo.jpg" width="30" height="35">
+          <img src="../assets/icons/norway.svg" alt="country-Norway" width="50" height="60">
+        </a>
+        <a class="navbar-item">
+          <img src="../assets/icons/uk.svg" alt="country-UK" width="50" height="60">
         </a>
       </div>
+    </div>
 
-      <div id="navbarBasic" class="navbar-menu">
-        <div class="navbar-start">
-          <a class="navbar-item is-uppercase is-size-3 has-text-gret-dark">POLITIVAKTA</a>
-        </div>
+  </nav>
+  <!-- ./NAVBAR -->
 
-        <div class="navbar-end">
-          <div class="navbar-item">
-            <div class="buttons">
-              <a class="navbar-item">
-                <img src="../assets/icons/norway.svg" alt="country-Norway" width="40" height="50">
-              </a>
-              <a class="navbar-item">
-                <img src="../assets/icons/uk.svg" alt="country-UK" width="40" height="50">
-              </a>
+  <div class="container">
+    <div class="block has-text-centered">
+      <button class="button is-success is-size-2 is-uppercase">Trekk kølapp</button>
+    </div>
+
+    <div class="notification is-info">
+      <button class="delete"></button>
+      <p class="has-text-centered is-size-5">Eller, gjør noen steg selv slik at vi kan hjelpe deg raskere! <br>
+        Begynn med å velge henvedelse under</p>
+    </div>
+
+    <div class="columns">
+      <div class="column">
+        <div class="box">
+          <article class="media">
+            <div class="media-left">
+              <span class="icon">
+                <i class="fas fa-passport is-size-3"></i>
+              </span>
             </div>
-          </div>
-        </div>
+            <div class="media-content">
+              <div class="content">
+                <p>
+                  <strong>Pass</strong>
+                  <br>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean efficitur sit amet massa fringilla
+                  egestas. Nullam condimentum luctus turpis.
+                </p>
+              </div>
+            </div>
+
+          </article>
+        </div> <!-- ./box -->
       </div>
+      <div class="column">
+        <div class="box">
+          <article class="media">
+            <div class="media-left">
+              <span class="icon">
+                <i class="fas fa-passport is-size-3"></i>
+              </span>
+            </div>
+            <div class="media-content">
+              <div class="content">
+                <p>
+                  <strong>Anmelde tyveri eller skadeverk</strong>
+                  <br>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean efficitur sit amet massa fringilla
+                  egestas. Nullam condimentum luctus turpis.
+                </p>
+              </div>
+            </div>
 
-    </nav>
-    <!-- ./NAVBAR -->
-
-    <div class="container">
-      <div class="block has-text-centered">
-        <button class="button is-success is-size-2 is-uppercase">Trekk kølapp</button>
+          </article>
+        </div> <!-- ./box -->
       </div>
+      <div class="column">
+        <div class="box">
+          <article class="media">
+            <div class="media-left">
+              <span class="icon">
+                <i class="fas fa-passport is-size-3"></i>
+              </span>
+            </div>
+            <div class="media-content">
+              <div class="content">
+                <p>
+                  <strong>Hente beslag</strong>
+                  <br>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean efficitur sit amet massa fringilla
+                  egestas. Nullam condimentum luctus turpis.
+                </p>
+              </div>
+            </div>
 
-      <div class="notification is-info">
-        <button class="delete"></button>
-        <p class="has-text-centered is-size-5">Eller, gjør noen steg selv slik at vi kan hjelpe deg raskere! <br>
-          Begynn med å velge henvedelse under</p>
+          </article>
+        </div> <!-- ./box -->
       </div>
+    </div>
 
-      <div class="columns">
-        <div class="column">
-          <div class="box">
-            <article class="media">
-              <div class="media-left">
-                <span class="icon">
-                  <i class="fas fa-passport is-size-3"></i>
-                </span>
+    <div class="columns">
+      <div class="column">
+        <div class="box">
+          <article class="media">
+            <div class="media-left">
+              <span class="icon">
+                <i class="fas fa-passport is-size-3"></i>
+              </span>
+            </div>
+            <div class="media-content">
+              <div class="content">
+                <p>
+                  <strong>Bot</strong>
+                  <br>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean efficitur sit amet massa fringilla
+                  egestas. Nullam condimentum luctus turpis.
+                </p>
               </div>
-              <div class="media-content">
-                <div class="content">
-                  <p>
-                    <strong>Pass</strong>
-                    <br>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean efficitur sit amet massa fringilla
-                    egestas. Nullam condimentum luctus turpis.
-                  </p>
-                </div>
-              </div>
+            </div>
 
-            </article>
-          </div> <!-- ./box -->
-        </div>
-        <div class="column">
-          <div class="box">
-            <article class="media">
-              <div class="media-left">
-                <span class="icon">
-                  <i class="fas fa-passport is-size-3"></i>
-                </span>
-              </div>
-              <div class="media-content">
-                <div class="content">
-                  <p>
-                    <strong>Anmelde tyveri eller skadeverk</strong>
-                    <br>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean efficitur sit amet massa fringilla
-                    egestas. Nullam condimentum luctus turpis.
-                  </p>
-                </div>
-              </div>
-
-            </article>
-          </div> <!-- ./box -->
-        </div>
-        <div class="column">
-          <div class="box">
-            <article class="media">
-              <div class="media-left">
-                <span class="icon">
-                  <i class="fas fa-passport is-size-3"></i>
-                </span>
-              </div>
-              <div class="media-content">
-                <div class="content">
-                  <p>
-                    <strong>Hente beslag</strong>
-                    <br>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean efficitur sit amet massa fringilla
-                    egestas. Nullam condimentum luctus turpis.
-                  </p>
-                </div>
-              </div>
-
-            </article>
-          </div> <!-- ./box -->
-        </div>
+          </article>
+        </div> <!-- ./box -->
       </div>
+      <div class="column">
+        <div class="box">
+          <article class="media">
+            <div class="media-left">
+              <span class="icon">
+                <i class="fas fa-passport is-size-3"></i>
+              </span>
+            </div>
+            <div class="media-content">
+              <div class="content">
+                <p>
+                  <strong>Våpen</strong>
+                  <br>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean efficitur sit amet massa fringilla
+                  egestas. Nullam condimentum luctus turpis.
+                </p>
+              </div>
+            </div>
 
-      <div class="columns">
-        <div class="column">
-          <div class="box">
-            <article class="media">
-              <div class="media-left">
-                <span class="icon">
-                  <i class="fas fa-passport is-size-3"></i>
-                </span>
+          </article>
+        </div> <!-- ./box -->
+      </div>
+      <div class="column">
+        <div class="box">
+          <article class="media">
+            <div class="media-left">
+              <span class="icon">
+                <i class="fas fa-passport is-size-3"></i>
+              </span>
+            </div>
+            <div class="media-content">
+              <div class="content">
+                <p>
+                  <strong>Avhør</strong>
+                  <br>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean efficitur sit amet massa fringilla
+                  egestas. Nullam condimentum luctus turpis.
+                </p>
               </div>
-              <div class="media-content">
-                <div class="content">
-                  <p>
-                    <strong>Bot</strong>
-                    <br>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean efficitur sit amet massa fringilla
-                    egestas. Nullam condimentum luctus turpis.
-                  </p>
-                </div>
-              </div>
+            </div>
 
-            </article>
-          </div> <!-- ./box -->
-        </div>
-        <div class="column">
-          <div class="box">
-            <article class="media">
-              <div class="media-left">
-                <span class="icon">
-                  <i class="fas fa-passport is-size-3"></i>
-                </span>
-              </div>
-              <div class="media-content">
-                <div class="content">
-                  <p>
-                    <strong>Våpen</strong>
-                    <br>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean efficitur sit amet massa fringilla
-                    egestas. Nullam condimentum luctus turpis.
-                  </p>
-                </div>
-              </div>
-
-            </article>
-          </div> <!-- ./box -->
-        </div>
-        <div class="column">
-          <div class="box">
-            <article class="media">
-              <div class="media-left">
-                <span class="icon">
-                  <i class="fas fa-passport is-size-3"></i>
-                </span>
-              </div>
-              <div class="media-content">
-                <div class="content">
-                  <p>
-                    <strong>Avhør</strong>
-                    <br>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean efficitur sit amet massa fringilla
-                    egestas. Nullam condimentum luctus turpis.
-                  </p>
-                </div>
-              </div>
-
-            </article>
-          </div> <!-- ./box -->
-        </div>
+          </article>
+        </div> <!-- ./box -->
       </div>
     </div>
   </div>
+</div>
 
 </template>
 
@@ -209,3 +210,7 @@ export default {
   }
 }
 </script>
+
+<style lang="scss">
+@import "../assets/sass/homepage.scss";
+</style>
